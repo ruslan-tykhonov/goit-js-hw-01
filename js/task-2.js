bd683d38e002db3a5d173a07e60e0000f663a25c
ruslan-tykhonov <RUSLANEGOROVICH1943@GMAIL.COM>
2024-09-20 09:00:47 +0200

    change

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-    return `Shipping to ${country} will cost ${prise + delivery} credits`;
+    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 
 }
 
